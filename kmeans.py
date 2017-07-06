5101b8db84cb7ab87d15c26016d79e696a3e0f2b
larinnim <larinnimalheiros@gmail.com>
2017-07-06 16:47:01 +0000

    Software

---
+++ kmeans.py
@@ -0,0 +1,18 @@
+import numpy as np
+from sklearn.cluster import KMeans
+import pandas as pd
+
+
+broken_df = pd.read_csv('dados.csv')
+mat = broken_df.as_matrix()
+kmeans = KMeans(n_clusters=3,random_state=3425).fit(mat)
+centroids = kmeans.cluster_centers_
+labels = kmeans.labels_
+
+def k_means_function(x):
+
+	Mean = x.mean(axis=0)
+	#print(Mean)
+	Y = kmeans.predict(Mean)
+
+	return Y
